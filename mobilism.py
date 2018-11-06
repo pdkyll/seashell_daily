8b35da1c6abf547bda206a1331ef277ac8c1a146
Richard Zhou <zsz2004@gmail.com>
2018-11-06 17:12:40 -0600

    20181106

--- mobilism.py
+++ mobilism.py
@@ -140,7 +140,7 @@ class SeashellMobilism:
         return not linkhref.startswith(self.lkws)
 
 
-mob = SeashellMobilism("Oct 30th, 2018, 8:42 pm")
+mob = SeashellMobilism("Nov 2nd, 2018, 5:06 pm")
 mob.process()
 # https://forum.mobilism.org/viewforum.php?f=399
-# https://forum.mobilism.org/viewtopic.php?f=427&t=2827377
+# https://forum.mobilism.org/viewtopic.php?f=1064&t=2834256
