a98939d6c362a5f6fbe10e6133e903e790969db0
seashell <zsz2004@gmail.com>
2019-03-15 21:18:27 -0500

    20190315 done 0day

--- zdaydown_t.py
+++ zdaydown_t.py
@@ -81,7 +81,7 @@ class Seashell0daydownT:
         f.write('\n')
 
 
-mob = Seashell0daydownT("https://www.0daydown.com/03/1006561.html")
+mob = Seashell0daydownT("https://www.0daydown.com/03/1011507.html")
 mob.process()
-mob = Seashell0daydownW("https://www.0daydown.com/03/896357.html")
+mob = Seashell0daydownW("https://www.0daydown.com/03/1011598.html")
 mob.process()
