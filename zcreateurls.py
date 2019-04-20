9bbfd4b9d61a5aabb58610e86dfc5b06cee781ff
zsz2004@gmail.com <Xdx=7719>
2019-04-20 02:46:51 -0500

    20190415 add url creator

--- zcreateurls.py
+++ zcreateurls.py
@@ -2,36 +2,27 @@
 #     print("http://xunleib.zuida360.com/1805/逆缘国语-"+str(i).zfill(2)+".mp4")
 
 
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.ui import WebDriverWait
+import asyncio
+from nonocaptcha.solver import Solver
 
-options = webdriver.ChromeOptions()
-options.binary_location = "C:/Users/seashell/Desktop/cr-stable/bin/chrome.exe"
-driver = webdriver.Chrome(options=options)
+pageurl = "https://www.google.com/recaptcha/api2/demo"
+sitekey = "6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-"
 
-driver.get(url='https://www.google.com/recaptcha/api2/demo')
-
-# find iframe
-captcha_iframe = WebDriverWait(driver, 10).until(
-    ec.presence_of_element_located(
-        (
-            By.TAG_NAME, 'iframe'
-        )
-    )
+proxy = "127.0.0.1:1000"
+auth_details = {
+    "username": "user",
+    "password": "pass"
+}
+args = ["--timeout 5"]
+options = {"ignoreHTTPSErrors": True, "args": args}
+client = Solver(
+    pageurl,
+    sitekey,
+    options=options,
+    proxy=proxy,
+    proxy_auth=auth_details,
 )
 
-ActionChains(driver).move_to_element(captcha_iframe).click().perform()
-
-# click im not robot
-captcha_box = WebDriverWait(driver, 10).until(
-    ec.presence_of_element_located(
-        (
-            By.ID, 'g-recaptcha-response'
-        )
-    )
-)
-
-driver.execute_script("arguments[0].click()", captcha_box)
+solution = asyncio.get_event_loop().run_until_complete(client.start())
+if solution:
+    print(solution)
